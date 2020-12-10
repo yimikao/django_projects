e2ed2861827c81ecbb78987ad28b4a79ad85516d
th_linux <holayinkajr@gmail.com>
2020-12-10 06:48:58 -0800

    update app views and url patterns

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -20,3 +20,22 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+#Use static() to add url mapping to serve statcic files during development(only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
