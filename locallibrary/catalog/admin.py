a45dadd5a31b64577d2800573f998d820bd95b89
th_linux <holayinkajr@gmail.com>
2020-12-12 12:58:28 -0800

    registered the models in the catalog admin file

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, BookInstance, Author, Genre, Language
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+
