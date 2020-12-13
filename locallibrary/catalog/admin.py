42209e030bba5825d0788c4c56b4a0f01a8eb886
th_linux <holayinkajr@gmail.com>
2020-12-13 14:08:30 -0800

    defined and registered model admin classes to custimize model admin interfaces

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -5,7 +5,24 @@ from .models import Book, BookInstance, Author, Genre, Language
 
 admin.site.register(Book)
 admin.site.register(BookInstance)
-admin.site.register(Author)
+#admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
 
+#Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+#Define and register the admin classes for Book using @register decorator(same as admin.site.reg())
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+#Define and register the admin classes for BookInstance using decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
